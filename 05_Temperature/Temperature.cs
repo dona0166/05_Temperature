eb7f64f759ef9fcbf7e0b11dbb511f08761b1ca1
Donnie <Donnie@DESKTOP-A0TIJCR>
2016-09-15 10:34:49 +0200

    Add project files.

---
+++ 05_Temperature/Temperature.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _05_Temperature
+{
+    public class Temperature
+    {
+        int c;
+        public static void Temperature(int f)
+        {
+            c = (5 / 9) * (f - 32);
+        }
+        public static int FahrenheitToCelcius(int v)
+        {
+           
+        }
+    }
+     
+    public class Main
+    {
+
+    }
+}
